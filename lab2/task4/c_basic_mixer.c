66f00743af00e3d83042a0c498e0ac15916d3a74
Bayaya <wangby00@gmail.com>
2020-02-08 17:09:09 -0500

    task4

--- lab2/task4/c_basic_mixer.c
+++ lab2/task4/c_basic_mixer.c
@@ -26,20 +26,21 @@ void conditionmixer() {
 	int i = 6;
 	char ch = 'm';
 	
-	printf("3==4 = %d\n", 3==4);
-	printf("3==3 = %d\n", 3==3);
-	printf("!0 = %d\n", !0);
-	printf("!(-1) = %d\n", !(-1));
-	printf("!\"abc\" =%d\n", !"abc");
-	printf("2.0f == 2.0 is %d\n", (2.0f == 2.0));
-	printf("1.1f == 1.1 is %d\n", (1.1f == 1.1));
+	printf("3==4 = %d\n", 3==4); 	// 3==4 = 0
+	printf("3==3 = %d\n", 3==3); 	// 3==3 = 1
+	printf("!0 = %d\n", !0);		// !0 = 1
+	printf("!(-1) = %d\n", !(-1));	//!(-1) = 0
+	printf("!\"abc\" =%d\n", !"abc");	// ! "abc" = 0
+	printf("2.0f == 2.0 is %d\n", (2.0f == 2.0));	//2.0f == 2.0 is 1
+	printf("1.1f == 1.1 is %d\n", (1.1f == 1.1));	//1.1f == 1.1 is 0 (b/c 1.1 is a rounding number in binary
+													// thus float 1.1 doess't equal to double 1.1)
 
-        printf("%d\n", ch < 'z' && ch > 'a');
-	printf("%d\n", !(ch-'z')?1:0);
-	printf("%d\n", 2 * 3 == 6 || (i = i + 1));
-        printf("%d\n", i);
-	printf("%d\n", 2 * 3 == 6 && (i = i + 1));
-        printf("%d\n", i);
+        printf("%d\n", ch < 'z' && ch > 'a');	//1
+	printf("%d\n", !(ch-'z')?1:0);				//0
+	printf("%d\n", 2 * 3 == 6 || (i = i + 1));	//1
+        printf("%d\n", i);						//7
+	printf("%d\n", 2 * 3 == 6 && (i = i + 1));	//0
+        printf("%d\n", i);						//8
 }
 
 void typemixer() {
@@ -50,9 +51,12 @@ void typemixer() {
 	// ---------------------------------------------------------------------
 	// The variables were already assigned values.  Let's check them out.
 	// ---------------------------------------------------------------------
-	printf ("i = %d \tj=%d \t\t k=%d\n", i, j, k);		
-	printf ("a = %c \tB=%c \t\t dollarSign=%c\n", a, B, dollarSign);		
-        printf ("x = %f \ty=%f \t\t z=%e\n", x, y, z);		
+	printf ("i = %d \tj=%d \t\t k=%d\n", i, j, k);	
+	//i = 1		j=10		k=20
+	printf ("a = %c \tB=%c \t\t dollarSign=%c\n", a, B, dollarSign);
+	//a = a 	B=B 		dollarSign=$		
+    printf ("x = %f \ty=%f \t\t z=%e\n", x, y, z);	
+    //x = 2.2	y=0.823529		z=1.96640
 	// ---------------------------------------------------------------------		
 	// And now the fun stuff ....
 	// ---------------------------------------------------------------------
@@ -61,25 +65,30 @@ void typemixer() {
 	// an integer plus a character
 
 	printf("a+j=%d\n", a+j);
+	//a+j=107 (ASCII code point of "a" plus 10)
 	// ---------------------------------------------------------------------
 	// a character plus an integer
 		
-        printf("k+B=%d\n", k+B);
+    printf("k+B=%d\n", k+B);
+    //k+B=86 (20 plus ASCII code point of "B")
 	// ---------------------------------------------------------------------
 	// mixing floats and characters
 		
 	printf("x/dollarSign = %f\n", x/dollarSign);
+	//x/dollarSign = 0.6111111 (2.2/36)
 	// ---------------------------------------------------------------------
 	// we can declare new objects at any time!
 		
 	char newChar;
-	newChar = a+j;
+	newChar = a+j; //newChar = k
 			
 	// ---------------------------------------------------------------------
 	// huh? What's going on here?
 		
 	printf("a+j=%c\n", newChar);
+	//a+j=k
 	printf("B-6=%d\n", B-6);
+	//B-6=60
 
 }
 int main( void )
