34d42b8a81c3553d6ecab85f69e24377fe126968
Bayaya <wangby00@gmail.com>
2020-02-16 19:16:34 -0500

    lab3 task4

--- lab3/task3/wordstats.c
+++ lab3/task3/wordstats.c
@@ -12,20 +12,20 @@ int main () {
   puts("Type some text (then ENTER):");
 
   do {
-  fgets (buf, MAX_BUF, stdin);
-  len = strlen(buf)-1;
-  //puts(buf);
+    fgets (buf, MAX_BUF, stdin);
+    len = strlen(buf) - 1;
+    //puts(buf);
 
-  do {
-    if(buf[len]>='A' && buf[len]<='Z'){
-      letter_frequency[buf[len]-'A'] += 1; 
-    } else if (buf[len]>='a' && buf[len]<='z') {
+    do {
+      if (buf[len] >= 'A' && buf[len] <= 'Z') {
+        letter_frequency[buf[len] - 'A'] += 1;
+      } else if (buf[len] >= 'a' && buf[len] <= 'z') {
 
-      letter_frequency[buf[len]-'a'] += 1;
-    }
-    len--;
-  } while (len > -1);
-} while (strlen(buf)-1 != 0);
+        letter_frequency[buf[len] - 'a'] += 1;
+      }
+      len--;
+    } while (len > -1);
+  } while (strlen(buf) - 1 != 0);
 
   printf("Distribution of letters in corpus:\n");
   for (i = 0; i < 26; i++) {
--- lab3/task4/rotate.c
+++ lab3/task4/rotate.c
@@ -1,19 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 void rotate_4(int *p_a, int*p_b, int*p_c, int* p_d) {
     //TO-DO: please implement left rotate 4 integers by one position
+
+    int temp;
+    temp = * p_a;
+    * p_a = * p_b;
+    * p_b = * p_c;
+    * p_c = * p_d;
+    * p_d = temp;
 }
 
-int main(int argc, char ** argv) {
+int main(int argc, char ** argv) { // char* = Array of chars = String; char ** = Array of Strings
+
     //TO-DO check if there are 4 inputs.
     //print out an error message if there are not enough inputs.
     //hint: use argc
-    int a = atoi(argv[1]); //atoi converts string to int
-    int b = atoi(argv[2]);
-    int c = atoi(argv[3]);
-    int d = atoi(argv[4]);
-    rotate_4(&a, &b, &c, &d);
-    printf("%d %d %d %d\n", a, b, c, d);
+
+    if (argc != 5) {
+        printf("%d\n", argc);
+        puts("Error: rotate needs four numbers");
+    } else {
+
+        int a = atoi(argv[1]); //atoi converts string to int
+        int b = atoi(argv[2]);
+        int c = atoi(argv[3]);
+        int d = atoi(argv[4]);
+
+        rotate_4(&a, &b, &c, &d);
+        printf("%d %d %d %d\n", a, b, c, d);
+
+    }
     return 0;
 }
