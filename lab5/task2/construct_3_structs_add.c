d891a9e5d35ec17dc901d94a418a7d54b20d31f5
Boyang Wang <wangby00@gmail.com>
2020-03-07 21:21:17 +0000

    lab5task2&3

--- lab5/task2/construct_3_structs_add.c
+++ lab5/task2/construct_3_structs_add.c
@@ -4,17 +4,58 @@
 typedef struct snode node_t;
 
 node_t * setup() {
-    //TODO:copy setup func from task1
+	node_t * head ;
+    head=(node_t*)malloc(sizeof(node_t));
+    node_t* node1;
+    node_t* node2;
+    node_t* node3;
+    node1 = head;
+    node1->length = 5;
+    char* str1 = "hello";
+    for(int i=0; i<node1->length+1; i++){
+            node1->str[i]=str1[i];}
+    node2=(node_t*) malloc(sizeof(node_t));
+    node1->next=node2;
+
+    node2->length = 5;
+    char* str2 = "there";
+    for(int i=0; i<node2->length+1; i++){
+            node2->str[i]=str2[i];}
+    node3=(node_t*) malloc(sizeof(node_t));
+    node2->next=node3;
+
+    node3->length = 4;
+    char* str3 = "prof";
+    for(int i=0; i<node3->length+1; i++){
+            node3->str[i]=str3[i];}
+    node3->next = NULL;
+
+   return head;
+
 }
 
-void teardown(/*what parameter?*/) {
+void teardown(node_t* head) {
+         node_t* trav;
+        trav = head;
+        while(trav!=NULL){
+                node_t* next=trav->next;
+                free(trav);
+                trav=next;
     //TODO: free all dynamic memory you requested.
     //Please complete the prototype of teardown.
     //You are not allowed to use globals
 }
+}
 
 void add(node_t ** head, char * str, int length){
     //TODO: implement add to add a new node to front, pointed by head
+    node_t* newnode = (node_t*) malloc (sizeof(node_t));
+    newnode->length = length;
+    for (int i=0; i<length+1; i++){
+    	newnode->str[i]=str[i];
+    }
+    newnode->next=*head;
+    *head = newnode;
 }
 
 //You can ignore the following code for testing
@@ -23,7 +64,7 @@ int main (int argc, char ** argv) {
     node_t * head = setup();
     add(&head, "hi", 2);
     dump_all(head);
-    teardown(/*what argument?*/);
+    teardown(head);
     return 0;
 }
 
--- lab5/task3/construct_3_structs_delete.c
+++ lab5/task3/construct_3_structs_delete.c
@@ -4,26 +4,94 @@
 typedef struct snode node_t;
 
 node_t * setup() {
+    node_t * head ;
+    head=(node_t*)malloc(sizeof(node_t));
+    node_t* node1;
+    node_t* node2;
+    node_t* node3;
+    node1 = head;
+    node1->length = 5;
+    char* str1 = "hello";
+    for(int i=0; i<node1->length+1; i++){
+            node1->str[i]=str1[i];}
+    node2=(node_t*) malloc(sizeof(node_t));
+    node1->next=node2;
+
+    node2->length = 5;
+    char* str2 = "there";
+    for(int i=0; i<node2->length+1; i++){
+            node2->str[i]=str2[i];}
+    node3=(node_t*) malloc(sizeof(node_t));
+    node2->next=node3;
+
+    node3->length = 4;
+    char* str3 = "prof";
+    for(int i=0; i<node3->length+1; i++){
+            node3->str[i]=str3[i];}
+    node3->next = NULL;
+
+   return head;
+
     //TODO:copy setup func from task2
 }
 
-void teardown(/*what parameter?*/) {
+void teardown(node_t* head) {
+         node_t* trav;
+        trav = head;
+        while(trav!=NULL){
+                node_t* next=trav->next;
+                free(trav);
+                trav=next;
     //TODO: free all dynamic memory you requested.
     //Please complete the prototype of teardown.
     //You are not allowed to use globals
+	}
 }
-
 void add(node_t ** head, char * str, int length){
+    node_t* newnode = (node_t*) malloc (sizeof(node_t));
+    newnode->length = length;
+    for (int i=0; i<length+1; i++){
+        newnode->str[i]=str[i];
+    }
+    newnode->next=*head;
+    *head = newnode;
     //TODO: copy add func from task2
 }
 void delete_node_at(node_t ** head, int idx) {
+	node_t* trav, *needfree, *pre;
+	trav = *head;
+	needfree = *head;
+	pre=*head;
+	if(idx == 0){
+	*head=(*head)->next;
+	free(needfree);
+	return;
+	}
+	for(int i =0; i<idx+1; i++){
+		if(trav==NULL){
+		printf("End of list\n");
+		return;
+		}else{
+	printf("i's position  %d\n", i);		
+	pre=needfree;
+	needfree=trav;
+	trav=trav->next;
+	}
+	}
+	pre->next=needfree->next;
+	free(needfree);
     //TODO: implement delete a node based on index
 	//deletes a node at index idx, which ranges from zero to the length of the list - 1.
-} 
+}
+
 void delete_node_key(node_t **head, char * key) {
-    //TODO: implement delete a node based on key
+             
+
+       
+	//TODO: implement delete a node based on key
 	//given a certain key, find and delete. 
 }
+
 //You can ignore the following code for testing
 void dump_all(node_t*);
 int main (int argc, char ** argv) {
@@ -32,7 +100,7 @@ int main (int argc, char ** argv) {
     delete_node_key(&head, "prof");
     delete_node_at(&head, 0);
     dump_all(head);
-    teardown(/*what argument?*/);
+    teardown(head);
     return 0;
 }
 
