c35bdc84d6b867e51fff097cd2d089103ee41353
Boyang Wang <wangby00@gmail.com>
2020-03-23 13:54:41 +0000

    lab6task2

---
+++ lab6/task2/snode.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "snode.h"
+#include <string.h>
+#include <stdlib.h>
+struct snode {
+  //TODO: change str to dynamic allcoation
+  char* str;
+  //int length;
+  struct snode *next;
+};
+
+struct snode *snode_create(char *s) {
+        struct snode * snode_ptr;
+	snode_ptr = (struct snode*)malloc(sizeof(struct snode));
+        char* create_s=(char*)malloc((strlen(s)+1)*sizeof(char));
+        strcpy(create_s,s);
+	snode_ptr->str = create_s;
+	snode_ptr->next=NULL;
+        return snode_ptr;
+
+ //TODO: implement snode_create, change the prototype to
+ //match with header file
+ //return node;
+  
+ }
+ 
+void snode_destroy(struct snode * s) {
+	free(s->str);
+        free(s);
+
+ //TODO: implement snode_destroy
+ 
+}
+
+char * snode_get_str(struct snode *s){
+return s->str;
+}
+
+struct snode * snode_get_next(struct snode *s){
+return s->next;
+}
+
